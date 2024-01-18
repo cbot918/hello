9cc2e3cfee12412f561b0936cb86fd148d6bef74
cbot918 <cbot918@gmail.com>
2024-01-19 00:03:59 +0800

    update

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-const port = ":8080"
-
 func main() {
+	port := ":8080"
+
+	if os.Getenv("PORT") != "" {
+		port = ":" + os.Getenv("PORT")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hihihihi")
 	})
